a71cc4d1cda13fa1e0856ac4306965c565d222b7
KIMSUBIN <KIMSUBIN@gmail.com>
2022-04-13 16:09:11 +0900

    0413-2

--- src/com/javaex/ex01/Ex01.java
+++ src/com/javaex/ex01/Ex01.java
@@ -27,6 +27,25 @@ public class Ex01 {
 		// long형은 int범위를 넘어가면 마지막에 L을 붙여줘야함.
 		System.out.println(no2); 
 		//long범위 -9223372036854775808 ~ 9223372036854775807
-	
+		
+		
+		//변수선언 초기화
+		
+		
+		 int var01;
+		 int var02;
+		 int var03;
+		/* int var01, var02, var03; */
+		
+		var01 = 10;
+		var02 = 20;
+		var03 = 30;
+		
+		//변수 여러개일 때 변수 + 초기화
+		int var04 = 10;
+		int var05 = 20;
+		int var06 = 30;
+		
+		System.out.println(var04);
 	}
 }
---
+++ src/com/javaex/ex01/Ex04.java
@@ -0,0 +1,22 @@
+package com.javaex.ex01;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		
+		char ch01 = 'A';
+		char ch02 = '안';
+		
+		System.out.println(ch01);
+		System.out.println(ch02);
+		
+		char ch03 = 65; // 코드 값 65가 우연히 A임...
+		System.out.println(ch03);
+		
+		char ch04 = '3'; //글자 3을 출력하고 싶으면 '3'으로 입력
+		System.out.println(ch04);
+		
+		String str = "안녕하세요"; //String 사용할 땐 큰따옴표 사용
+		System.out.println(str);
+	}
+}
